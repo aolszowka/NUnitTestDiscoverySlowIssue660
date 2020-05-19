de9a08d47ff54d4f447de5d0bf5a1e9b606667c6
Ace Olszowka <aolszowka@gmail.com>
2020-05-19 08:39:34 -0700

    Initial Commit

---
+++ TestLibrary1/TestLibrary1.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary1
+{
+    [TestFixture]
+    public class TestLibrary1
+    {
+        [TestCaseSource(nameof(TestLibrary1.Tests))]
+        public void BlahTest()
+        {
+            Assert.True(true);
+        }
+
+        public static IEnumerable<TestCaseData> Tests()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                yield return new TestCaseData().SetName($"TestNumber{i}");
+            }
+        }
+    }
+}
